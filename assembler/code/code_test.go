d7f82141f88a61d70867174b913d3caf3a1f969e
kimuson <redram342@gmail.com>
2023-03-12 17:58:46 +0900

    test: add code.CCommand.Convert

--- assembler/code/code_test.go
+++ assembler/code/code_test.go
@@ -6,7 +6,48 @@ import (
 )
 
 func TestCCommand_Convert(t *testing.T) {
+	const wantErr, noErr = true, false
+	testCases := map[string]struct {
+		dest    *code.Dest
+		comp    code.Comp
+		jump    *code.Jump
+		want    string
+		wantErr bool
+	}{
+		"D=M":      {ptr(code.DEST_D), code.COMP_M, nil, "1111110000010000", noErr},
+		"D=D-A":    {ptr(code.DEST_D), code.COMP_D_MINUS_A, nil, "1110010011010000", noErr},
+		"D=M;JMP":  {ptr(code.DEST_D), code.COMP_M, ptr(code.JUMP), "1111110000010111", noErr},
+		"D;JGT":    {nil, code.COMP_D, ptr(code.JUMP_GREATER_THAN), "1110001100000001", noErr},
+		"hoge=M+1": {ptr(code.Dest("hoge")), code.COMP_M_ADD_1, nil, "", wantErr},
+		"M=hoge":   {ptr(code.DEST_M), code.Comp("hoge"), nil, "", wantErr},
+		"M;hoge":   {nil, code.COMP_M, ptr(code.Jump("hoge")), "", wantErr},
+	}
 
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			cCommand := code.CCommand{
+				Dest: tc.dest,
+				Comp: tc.comp,
+				Jump: tc.jump,
+			}
+
+			got, err := cCommand.Convert()
+			if err != nil && !tc.wantErr {
+				t.Error(err)
+			}
+
+			if err == nil && tc.wantErr {
+				t.Error("no error")
+			}
+
+			if got != tc.want {
+				t.Errorf("got = %v, but got = %v", tc.want, got)
+			}
+		})
+	}
 }
 
 func TestACommand_Convert(t *testing.T) {
