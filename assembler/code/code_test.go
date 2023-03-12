a107d57b32f8361320f0694e5c4968fbbd94b606
kimuson <redram342@gmail.com>
2023-03-12 13:27:25 +0900

    test: add a_command.Convert test

---
+++ assembler/code/code_test.go
@@ -0,0 +1,31 @@
+package code_test
+
+import (
+	"assembler/code"
+	"testing"
+)
+
+func TestACommand_Convert(t *testing.T) {
+	testCases := map[string]struct {
+		symbol string
+		value  int
+		want   string
+	}{
+		"@100": {"", 100, "0000000001100100"},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			aCommand := code.ACommand{
+				Symbol: tc.symbol,
+				Value:  tc.value,
+			}
+
+			if got := aCommand.Convert(); got != tc.want {
+				t.Errorf("want:\n%v\ngot:\n%v", tc.want, got)
+			}
+		})
+	}
+}
