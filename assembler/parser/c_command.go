5d02c2f13f915e220d68fb0313e8a59b87e3bbf1
kimuson <redram342@gmail.com>
2023-03-12 09:08:59 +0900

    feat: impl raw command to code.Dest, code.Comp, code.Jump

--- assembler/parser/c_command.go
+++ assembler/parser/c_command.go
@@ -17,6 +17,9 @@ const (
 
 var (
 	ErrCanNotParseCCommand = errors.New("can not parse to CCommand")
+	ErrNoSuchComp          = errors.New("no such a comp kind")
+	ErrNoSuchDest          = errors.New("no such a dest kind")
+	ErrNoSuchJump          = errors.New("no such a jump kind")
 )
 
 type CCommand struct {
@@ -54,6 +57,93 @@ func isCCommand(raw string) bool {
 
 func (c *CCommand) parse() code.Command {
 	return nil
+}
+
+func mapCodeDest(raw string) (*code.Dest, error) {
+	mp := map[string]code.Dest{
+		"M":   code.DEST_M,
+		"D":   code.DEST_D,
+		"MD":  code.DEST_MD,
+		"A":   code.DEST_A,
+		"AM":  code.DEST_AM,
+		"AD":  code.DEST_AD,
+		"AMD": code.DEST_AMD,
+	}
+
+	if raw == "" {
+		return nil, nil
+	}
+
+	dest, ok := mp[raw]
+	if !ok {
+		return nil, ErrNoSuchDest
+	}
+
+	return &dest, nil
+}
+
+func mapCodeComp(raw string) (*code.Comp, error) {
+	mp := map[string]code.Comp{
+		"0":   code.COMP_0,
+		"1":   code.COMP_1,
+		"-1":  code.COMP_MINUS_1,
+		"D":   code.COMP_D,
+		"A":   code.COMP_A,
+		"!D":  code.COMP_NOT_A,
+		"!A":  code.COMP_NOT_D,
+		"-A":  code.COMP_MINUS_A,
+		"-D":  code.COMP_MINUS_D,
+		"D+1": code.COMP_D_ADD_1,
+		"A+1": code.COMP_A_ADD_1,
+		"D-1": code.COMP_D_MINUS_1,
+		"A-1": code.COMP_A_MINUS_1,
+		"D+A": code.COMP_D_ADD_A,
+		"D-A": code.COMP_D_MINUS_A,
+		"A-D": code.COMP_A_MINUS_D,
+		"D&A": code.COMP_D_AND_A,
+		"D|A": code.COMP_D_OR_A,
+		"M":   code.COMP_M,
+		"!M":  code.COMP_NOT_M,
+		"M+1": code.COMP_M_ADD_1,
+		"D+M": code.COMP_D_ADD_M,
+		"D-M": code.COMP_D_MINUS_A,
+		"D&M": code.COMP_D_AND_M,
+		"D|M": code.COMP_D_OR_M,
+	}
+
+	if raw == "" {
+		return nil, nil
+	}
+
+	comp, ok := mp[raw]
+	if !ok {
+		return nil, ErrNoSuchComp
+	}
+
+	return &comp, nil
+}
+
+func mapCodeJump(raw string) (*code.Jump, error) {
+	mp := map[string]code.Jump{
+		"JGT": code.JUMP_GREATER_THAN,
+		"JEQ": code.JUMP_EQUAL,
+		"JGE": code.JUMP_GREATER_EQUAL,
+		"JLT": code.JUMP_LOWER_THAN,
+		"JNE": code.JUMP_NOT_EQUAL,
+		"JLE": code.JUMP_LOWER_EQUAL,
+		"JMP": code.JUMP,
+	}
+
+	if raw == "" {
+		return nil, nil
+	}
+
+	jump, ok := mp[raw]
+	if !ok {
+		return nil, ErrNoSuchJump
+	}
+
+	return &jump, nil
 }
 
 func toCCommand(raw string) (*CCommand, error) {
