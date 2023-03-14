5de4524f25b895a18e5dff6de2fb3b572fc8c4d1
kimuson <redram342@gmail.com>
2023-03-15 08:25:22 +0900

    feat: impl codeWriter.Write method

--- vmtranslator/codewriter/write.go
+++ vmtranslator/codewriter/write.go
@@ -24,3 +24,22 @@ func New(commands []Command, fileName string) (CodeWriter, error) {
 
 	return codeWriter, nil
 }
+
+func (c CodeWriter) Write() error {
+	for _, command := range c.commands {
+		b, err := command.convert()
+		if err != nil {
+			return fmt.Errorf("write error: %w", err)
+		}
+
+		if _, err := c.file.Write(b); err != nil {
+			return fmt.Errorf("write error: %w", err)
+		}
+	}
+
+	if err := c.file.Close(); err != nil {
+		return fmt.Errorf("write error: %w", err)
+	}
+
+	return nil
+}
