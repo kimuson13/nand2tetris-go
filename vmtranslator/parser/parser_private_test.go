01714f01d04391678bec062b74a5b792691cdf0c
kimuson <redram342@gmail.com>
2023-03-14 18:09:09 +0900

    test: add private parser test

--- vmtranslator/parser/parser_private_test.go
+++ vmtranslator/parser/parser_private_test.go
@@ -6,6 +6,112 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+func TestNew(t *testing.T) {
+	in := "// comment \n\npush constant 6\nadd // comment"
+	want := Parser{
+		commands:       s("push constant 6", "add"),
+		currentCommand: s("push", "constant", "6"),
+	}
+
+	got, err := New(in)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if diff := cmp.Diff(want, got, cmp.AllowUnexported(Parser{})); diff != "" {
+		t.Errorf("in: %s\n%s", in, diff)
+	}
+}
+
+func TestGetCommands(t *testing.T) {
+	const wantErr, noErr = true, false
+	testCases := map[string]struct {
+		in      string
+		want    []string
+		wantErr bool
+	}{
+		"get_commands":   {"// comment\n\nadd // comment", s("add"), noErr},
+		"invalid_syntax": {"add // comment // comment", []string{}, wantErr},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := getCommands(tc.in)
+			if err != nil && !tc.wantErr {
+				t.Error(err)
+			}
+
+			if err == nil && tc.wantErr {
+				t.Errorf("no err: %v", tc.in)
+			}
+
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestGetCommand(t *testing.T) {
+	const wantErr, noErr = true, false
+	testCases := map[string]struct {
+		in      string
+		want    string
+		wantErr bool
+	}{
+		"command":              {"add", "add", noErr},
+		"comment_line":         {"// comment", "", noErr},
+		"empty_line":           {"", "", noErr},
+		"command_with_comment": {"push constant 6 // comment", "push constant 6", noErr},
+		"too_many_comment_lit": {"add // comment // comment", "", wantErr},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := getCommand(tc.in)
+			if err != nil && !tc.wantErr {
+				t.Error(err)
+			}
+
+			if err == nil && tc.wantErr {
+				t.Errorf("no err: %v", tc.in)
+			}
+
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestIsNotCommand(t *testing.T) {
+	testCases := map[string]struct {
+		in   string
+		want bool
+	}{
+		"empty_line":   {"", true},
+		"comment_line": {"// comment", true},
+		"command_line": {"push constant 6", false},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+		})
+
+		if got := isNotCommand(tc.in); got != tc.want {
+			t.Errorf("want = %v, but got =%v", tc.want, got)
+		}
+	}
+}
+
 func TestTrimComment(t *testing.T) {
 	const wantErr, noErr = true, false
 	testCases := map[string]struct {
@@ -38,6 +144,10 @@ func TestTrimComment(t *testing.T) {
 			}
 		})
 	}
+}
+
+func s[T any](val ...T) []T {
+	return val
 }
 
 // test format
