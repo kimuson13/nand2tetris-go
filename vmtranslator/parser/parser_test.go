00a61c08cbd7f3f90f45ea2ff1682b9b51d3f15c
kimuson <redram342@gmail.com>
2023-03-14 18:10:45 +0900

    test: add parser.New public test

--- vmtranslator/parser/parser_test.go
+++ vmtranslator/parser/parser_test.go
@@ -1,7 +1,14 @@
 package parser_test
 
-import "testing"
+import (
+	"testing"
+	"vmtranslator/parser"
+)
 
 func TestNew(t *testing.T) {
+	in := "// comment \n\npush constant 6\nadd // comment"
 
+	if _, err := parser.New(in); err != nil {
+		t.Error(err)
+	}
 }
